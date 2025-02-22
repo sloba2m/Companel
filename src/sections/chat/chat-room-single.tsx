d8b5b9c7304c51c0f957df77feb7ea867bc16bc7
Nemanja <necer.radoicic@gmail.com>
2025-02-22 14:45:37 +0100

    right sidebar part done

--- src/sections/chat/chat-room-single.tsx
+++ src/sections/chat/chat-room-single.tsx
@@ -2,8 +2,19 @@ import type { IChatParticipant } from 'src/types/chat';
 
 import Stack from '@mui/material/Stack';
 import Avatar from '@mui/material/Avatar';
-import Collapse from '@mui/material/Collapse';
 import Typography from '@mui/material/Typography';
+import {
+  Box,
+  Chip,
+  List,
+  Button,
+  ListItem,
+  Collapse,
+  TextField,
+  ListItemIcon,
+  ListItemText,
+  ListItemButton,
+} from '@mui/material';
 
 import { useBoolean } from 'src/hooks/use-boolean';
 
@@ -18,39 +29,27 @@ type Props = {
 };
 
 export function ChatRoomSingle({ participant }: Props) {
-  const collapse = useBoolean(true);
+  const collapseTag = useBoolean(true);
+  const collapseConv = useBoolean(true);
 
   const renderInfo = (
-    <Stack alignItems="center" sx={{ py: 5 }}>
-      <Avatar
-        alt={participant?.name}
-        src={participant?.avatarUrl}
-        sx={{ width: 96, height: 96, mb: 2 }}
-      />
+    <Stack alignItems="center" direction="row" justifyContent="center" sx={{ p: 3, gap: 2 }}>
+      <Avatar alt={participant?.name} src={participant?.avatarUrl} sx={{ width: 48, height: 48 }} />
       <Typography variant="subtitle1">{participant?.name}</Typography>
-      <Typography variant="body2" sx={{ color: 'text.secondary', mt: 0.5 }}>
-        {participant?.role}
-      </Typography>
     </Stack>
   );
 
   const renderContact = (
     <Stack spacing={2} sx={{ px: 2, py: 2.5 }}>
-      {[
-        { icon: 'mingcute:location-fill', value: participant?.address },
-        { icon: 'solar:phone-bold', value: participant?.phoneNumber },
-        { icon: 'fluent:mail-24-filled', value: participant?.email },
-      ].map((item) => (
-        <Stack
-          key={item.icon}
-          spacing={1}
-          direction="row"
-          sx={{ typography: 'body2', wordBreak: 'break-all' }}
-        >
-          <Iconify icon={item.icon} sx={{ flexShrink: 0, color: 'text.disabled' }} />
-          {item.value}
-        </Stack>
-      ))}
+      <Typography>Contact information</Typography>
+      <TextField fullWidth label="Address" defaultValue={participant?.address} />
+      <TextField
+        fullWidth
+        label="Phone number"
+        defaultValue={participant?.phoneNumber}
+        type="tel"
+      />
+      <TextField fullWidth label="Email" defaultValue={participant?.email} type="email" />
     </Stack>
   );
 
@@ -58,11 +57,68 @@ export function ChatRoomSingle({ participant }: Props) {
     <>
       {renderInfo}
 
-      <CollapseButton selected={collapse.value} onClick={collapse.onToggle}>
-        Information
+      {renderContact}
+      <CollapseButton selected={collapseTag.value} onClick={collapseTag.onToggle}>
+        Tags
       </CollapseButton>
 
-      <Collapse in={collapse.value}>{renderContact}</Collapse>
+      <Collapse in={collapseTag.value}>
+        <Stack spacing={2} sx={{ px: 2, py: 2.5 }}>
+          <Box sx={{ display: 'flex', gap: 1 }}>
+            <Chip
+              variant="soft"
+              label="tag 1"
+              size="small"
+              onDelete={() => console.log('delete')}
+            />
+            <Chip
+              variant="soft"
+              label="tag 2"
+              size="small"
+              onDelete={() => console.log('delete')}
+            />
+          </Box>
+          <Box sx={{ display: 'flex', gap: 2 }}>
+            <TextField fullWidth label="New tag" size="small" />
+            <Button variant="soft">Add</Button>
+          </Box>
+        </Stack>
+      </Collapse>
+
+      <CollapseButton selected={collapseConv.value} onClick={collapseConv.onToggle}>
+        Previous conversation
+      </CollapseButton>
+
+      <Collapse in={collapseConv.value}>
+        <Box sx={{ bgcolor: 'background.paper' }}>
+          <nav aria-label="main mailbox folders">
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <Iconify icon="mdi:message-arrow-left-outline" width={16} />
+                  </ListItemIcon>
+                  <ListItemText primary="Conversation 1" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <Iconify icon="mdi:message-arrow-left-outline" width={16} />
+                  </ListItemIcon>
+                  <ListItemText primary="Conversation 2" />
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </nav>
+        </Box>
+      </Collapse>
+
+      <CollapseButton selected={collapseConv.value} onClick={collapseConv.onToggle}>
+        Event history
+      </CollapseButton>
+
+      <Collapse in={collapseConv.value} />
     </>
   );
 }
