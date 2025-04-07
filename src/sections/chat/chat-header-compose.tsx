846e5238689942011aeee6685e01be628a828099
Nemanja <necer.radoicic@gmail.com>
2025-04-07 22:22:13 +0200

    more files removed

--- src/sections/chat/chat-header-compose.tsx
+++ src/sections/chat/chat-header-compose.tsx
@@ -3,9 +3,9 @@ import type { WorkspaceInbox } from 'src/actions/account';
 import { useTranslation } from 'react-i18next';
 
 import Box from '@mui/material/Box';
-import { IconButton, Stack, Grid } from '@mui/material';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import { Stack, IconButton } from '@mui/material';
 import Autocomplete from '@mui/material/Autocomplete';
 
 import { useResponsive } from 'src/hooks/use-responsive';
@@ -56,7 +56,16 @@ export function ChatHeaderCompose({ onOpenMobile, onChange, values }: Props) {
               {t('conversations.createForm.inbox')}:
             </Typography>
           </Box>
-          <Box sx={{ display: 'flex', flexDirection: 'column', flexWrap: 'wrap', gap: 2, width: '100%', mr: 2 }}>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              flexWrap: 'wrap',
+              gap: 2,
+              width: '100%',
+              mr: 2,
+            }}
+          >
             <Autocomplete
               fullWidth
               sx={{ flex: 1 }}
@@ -70,23 +79,26 @@ export function ChatHeaderCompose({ onOpenMobile, onChange, values }: Props) {
                 <TextField {...params} label="Inbox" margin="none" size="small" />
               )}
             />
-
           </Box>
         </Box>
         <Box display="flex" sx={{ flex: 1, width: '100%' }} gap={2}>
           <Box display="flex" sx={{ flex: 1 }}>
-            <Box sx={{ display: 'flex', flexDirection: 'column', flexWrap: 'wrap', gap: 2, flex: 0 }}>
-              {[
-                t('conversations.createForm.to'),
-                'Cc',
-              ].map((label) => (
-                <Typography key={label} variant="subtitle2" sx={{ color: 'text.primary', my: 1.1, width: '40px' }}>
+            <Box
+              sx={{ display: 'flex', flexDirection: 'column', flexWrap: 'wrap', gap: 2, flex: 0 }}
+            >
+              {[t('conversations.createForm.to'), 'Cc'].map((label) => (
+                <Typography
+                  key={label}
+                  variant="subtitle2"
+                  sx={{ color: 'text.primary', my: 1.1, width: '40px' }}
+                >
                   {label}:
                 </Typography>
               ))}
             </Box>
-            <Box sx={{ display: 'flex', flexDirection: 'column', flexWrap: 'wrap', gap: 2, flex: 1 }}>
-
+            <Box
+              sx={{ display: 'flex', flexDirection: 'column', flexWrap: 'wrap', gap: 2, flex: 1 }}
+            >
               <TextField
                 placeholder={`+ ${t('conversations.new.recipients')}`}
                 size="small"
@@ -101,23 +113,28 @@ export function ChatHeaderCompose({ onOpenMobile, onChange, values }: Props) {
                 onChange={(e) => onChange('cc', e.target.value)}
                 sx={{ wdith: '100%', ml: 2 }}
               />
-
             </Box>
           </Box>
           <Box display="flex" sx={{ flex: 1 }} pr={2}>
-            <Box sx={{ display: 'flex', flexDirection: 'column', flexWrap: 'wrap', gap: 2, flex: 0 }}>
+            <Box
+              sx={{ display: 'flex', flexDirection: 'column', flexWrap: 'wrap', gap: 2, flex: 0 }}
+            >
               {[
-
                 t('conversations.createForm.contactName'),
                 t('conversations.createForm.subject'),
               ].map((label) => (
-                <Typography key={label} variant="subtitle2" sx={{ color: 'text.primary', my: 1.1, width: '60px' }}>
+                <Typography
+                  key={label}
+                  variant="subtitle2"
+                  sx={{ color: 'text.primary', my: 1.1, width: '60px' }}
+                >
                   {label}:
                 </Typography>
               ))}
             </Box>
-            <Box sx={{ display: 'flex', flexDirection: 'column', flexWrap: 'wrap', gap: 2, flex: 1 }}>
-
+            <Box
+              sx={{ display: 'flex', flexDirection: 'column', flexWrap: 'wrap', gap: 2, flex: 1 }}
+            >
               <TextField
                 placeholder="Name"
                 size="small"
@@ -134,11 +151,8 @@ export function ChatHeaderCompose({ onOpenMobile, onChange, values }: Props) {
               />
             </Box>
           </Box>
-
         </Box>
-
-      </Stack >
-
+      </Stack>
     </>
   );
 }
